f357d9edcb89c852532b82a299c4ab7d15e79ff8
Winston Howard <winston.howard@yahoo.com>
2020-02-18 10:28:52 -0700

    Initalized Project

---
+++ pe_scanner.py
@@ -0,0 +1,16 @@
+"""
+pe_scanner
+Description: A basic python script to scan through a directory of files and create a report of all those files
+Author: Winston Howard
+Created Date: 02/18/20
+"""
+import subprocess
+import pefile
+
+
+def scanner():
+    print("A stub function for the scanner")
+    pass
+
+
+scanner()
