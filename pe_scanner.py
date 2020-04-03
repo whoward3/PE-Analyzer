3fed0a25e21dfdd381f86e133cc905b7a4c62616
Winston Howard <winston.howard@yahoo.com>
2020-04-03 14:09:49 -0600

    Update pe_scanner.py

--- pe_scanner.py
+++ pe_scanner.py
@@ -1,7 +1,7 @@
 """
 pe_scanner
 Description: A basic python script to scan through a directory of files and create a report of all those files
-Author: Winston Howard
+Authors: Winston Howard, Alice Blair, Chance Sweetser
 Created Date: 02/18/20
 """
 import subprocess
@@ -15,6 +15,7 @@ def scanner():
     The scanner function used to scan directory of files
     """
     path = input('Path to PE File Directory: ')
+    print(path)
     for filename in os.listdir(path):
         print("\n {} : \r".format(filename))
         try:
@@ -30,7 +31,9 @@ def reporter(pe):
     The reporter function that will return a report including a reference to virus total via:
     https://www.virustotal.com/gui/search/{HASH}
     """
+
     pass
 
 
+# C:\Program Files (x86)\Steam\steamapps\common\Star Wars Empire at War\corruption\
 scanner()
